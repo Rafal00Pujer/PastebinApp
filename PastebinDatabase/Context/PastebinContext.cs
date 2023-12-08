f5444aa2733abfa173e52d4aef8c0122df2d4237
RafalPujer <rafal.pujer00@gmail.com>
2023-12-08 15:08:32 +0100

    Added database context and connection string

---
+++ PastebinDatabase/Context/PastebinContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PastebinDatabase.Entities;
+
+namespace PastebinDatabase.Context;
+
+public class PastebinContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<PasteEntity> Pastes { get; set; }
+
+    public DbSet<PasteMetaEntity> PastesMetas { get; set; }
+
+    public DbSet<PastePasswordEntity> PastesPasswords { get; set; }
+}
