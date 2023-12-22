27ffb804f42255fc6dd00a01b7957dcbc4fd3bd3
RafalPujer <rafal.pujer00@gmail.com>
2023-12-22 17:25:55 +0100

    Update relation on meta and password

---
+++ PastebinDatabase/Migrations/20231222162350_SetDeleteToCascadeOnMetaAndPasswordRelation.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PastebinDatabase.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetDeleteToCascadeOnMetaAndPasswordRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pastes_Passwords_Pastes_Metas_PasteId",
+                table: "Pastes_Passwords");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pastes_Passwords_Pastes_Metas_PasteId",
+                table: "Pastes_Passwords",
+                column: "PasteId",
+                principalTable: "Pastes_Metas",
+                principalColumn: "PasteId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pastes_Passwords_Pastes_Metas_PasteId",
+                table: "Pastes_Passwords");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pastes_Passwords_Pastes_Metas_PasteId",
+                table: "Pastes_Passwords",
+                column: "PasteId",
+                principalTable: "Pastes_Metas",
+                principalColumn: "PasteId");
+        }
+    }
+}
